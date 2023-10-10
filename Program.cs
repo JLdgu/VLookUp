eb12b3102867ef850471a9849be12a08386b80ec
JLdgu <jonathan.linstead@devon.gov.uk>
2023-10-10 14:38:26 +0100

    some changes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,27 @@
 ﻿using ClosedXML.Excel;
 
-const string vlookup_workbook = @"C:\Users\Jonathan.Linstead\OneDrive - Devon County Council\Phones\Mobile phone export excluding status of Disposed 050623.xlsx";
+const string vlookup_workbook = @"C:\Users\Jonathan.Linstead\OneDrive - Devon County Council\EUCSharepoint\Mobile Phones\iPad for reuse and disposal.xlsx";
+const string vSearch_workbook = @"C:\Users\Jonathan.Linstead\Downloads\CI List2023_08_8_09_16_04.xlsx";
 
-const string lookup_worksheet = "SR204153";
-Range lookup = new(2, 2, 944); // 944
-const int output_column = 6; 
+const string lookup_worksheet = "Sheet1";
+Range lookup = new(2, 13, 15); // Column to lookup, Start Row, End Row
+const int output_column = 12; 
 
 const string search_worksheet = "Data";
-Range search = new(1, 2, 11705);  // Column to search, Start Row, End
-const int result_column = 3;
-bool deleteMissingRows = true;
+Range search = new(11, 56, 58);  // Column to search, Start Row, End Row
+const int result_column = 4;
+bool deleteUnmatchRows = false;
+bool deleteOnly = false;
 
 using XLWorkbook workbook = new(vlookup_workbook);
+using XLWorkbook searchBook = new(vSearch_workbook);
 
 IXLWorksheet lookupWorksheet = workbook.Worksheet(lookup_worksheet);
-IXLWorksheet searchWorksheet = workbook.Worksheet(search_worksheet);
+IXLWorksheet searchWorksheet = searchBook.Worksheet(search_worksheet);
+
+Console.WriteLine( "Look Column: {0}",lookupWorksheet.Cell(1, lookup.Column).Value.ToString());
+Console.WriteLine( "Search Column: {0}",searchWorksheet.Cell(1, search.Column).Value.ToString());
+Console.WriteLine( "Result Column: {0}",searchWorksheet.Cell(1, result_column).Value.ToString());
 
 for (int row = lookup.EndRow; row > lookup.StartRow - 1; row--)
 {
@@ -22,13 +29,17 @@ for (int row = lookup.EndRow; row > lookup.StartRow - 1; row--)
     string? result = Search(imei);
     if (result is null)
     {
-        if (deleteMissingRows)
+        if (deleteUnmatchRows)
             lookupWorksheet.Row(row).Delete();
     }
     else
-        lookupWorksheet.Cell(row, output_column).Value = result;
+    {
+        if (!deleteOnly)
+            lookupWorksheet.Cell(row, output_column).Value = result;
+    }
 }
 workbook.Save();
+
 
 string? Search(string imei)
 {
@@ -40,4 +51,4 @@ string? Search(string imei)
     return null;
 }
 
-public record Range(int Column, int StartRow, int EndRow);
+record Range(int Column, int StartRow, int EndRow);
