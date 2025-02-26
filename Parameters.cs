3320a2a4e3049ecd251a0e33504bbe26b4e27060
JLdgu <jonathan.linstead@devon.gov.uk>
2025-02-26 11:08:22 +0000

    Use JSON file for parameters

---
+++ Parameters.cs
@@ -0,0 +1,17 @@
+﻿namespace VLookUp;
+
+internal class Parameters
+{
+    public required string LookupWorkbook { get; set; }
+    public required string LookupWorksheet { get; set; }
+    public required string LookupColumn { get; set; }
+    public required string LookupOutputColumn { get; set; }
+    public required int LookupStartRow { get; set; }
+    public required int LookupEndRow { get; set; }
+    public required string SearchWorkbook { get; set; }
+    public required string SearchWorksheet { get; set; }
+    public required string SearchColumn { get; set; }
+    public required string SearchResultColumn { get; set; }
+    public required int SearchStartRow { get; set; }
+    public required int SearchEndRow { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,60 +1,70 @@
 ﻿using ClosedXML.Excel;
+using System.Text.Json;
+using VLookUp;
 
-const string vlookup_workbook = @"C:\temp\disposals.xlsx";
-const string vSearch_workbook = @"C:\temp\CI List2023_10_10_13_14_47.xlsx";
-
-const string lookup_worksheet = "disposals";
-Range lookup = new(1, 2, 39); // Column to lookup (A = 1), Start Row, End Row
-const int output_column = 10; 
-
-const string search_worksheet = "Data";
-Range search = new(4, 2, 4650);  // Column to search, Start Row, End Row
-const int result_column = 2;
 bool deleteUnmatchRows = false;
 bool deleteOnly = false;
 
-using XLWorkbook workbook = new(vlookup_workbook);
-using XLWorkbook searchBook = new(vSearch_workbook);
+string fileName = "parameters.json";
+string jsonString = File.ReadAllText(fileName);
 
-IXLWorksheet lookupWorksheet = workbook.Worksheet(lookup_worksheet);
-IXLWorksheet searchWorksheet = searchBook.Worksheet(search_worksheet);
+try
+{
+    Parameters? parameters = JsonSerializer.Deserialize<Parameters>(jsonString);
 
-Console.WriteLine( "Look Column: {0}",lookupWorksheet.Cell(1, lookup.Column).Value.ToString());
-Console.WriteLine( "Search Column: {0}",searchWorksheet.Cell(1, search.Column).Value.ToString());
-Console.WriteLine( "Result Column: {0}",searchWorksheet.Cell(1, result_column).Value.ToString());
+    if (parameters == null) return; // Serialize will throw exception if expected parameters are missing
 
-Console.WriteLine("Are these the correct columns? (y/n)");
-var yn = Console.ReadKey();
+    using XLWorkbook workbook = new(parameters.LookupWorkbook);
+    using XLWorkbook searchBook = new(parameters.SearchWorkbook);
 
-if (yn.Key.ToString()  == "n" || yn.Key.ToString() == "N" || yn.Key == ConsoleKey.Escape)        
+    IXLWorksheet lookupWorksheet = workbook.Worksheet(parameters.LookupWorksheet);
+    IXLWorksheet searchWorksheet = searchBook.Worksheet(parameters.SearchWorksheet);
+
+    Console.WriteLine("Search spreadsheet last row used {0}", lookupWorksheet.LastRowUsed());
+
+    Console.WriteLine("Look Column: {0}", lookupWorksheet.Cell(1, parameters.LookupColumn).Value.ToString());
+    Console.WriteLine("Output Column: {0}", lookupWorksheet.Cell(1, parameters.LookupOutputColumn).Value.ToString());
+    Console.WriteLine("Search Column: {0}", searchWorksheet.Cell(1, parameters.SearchColumn).Value.ToString());
+    Console.WriteLine("Result Column: {0}", searchWorksheet.Cell(1, parameters.SearchResultColumn).Value.ToString());
+
+    Console.WriteLine("Are these the correct columns? (y/n)");
+    var yn = Console.ReadKey();
+
+    if (yn.Key.ToString() == "n" || yn.Key.ToString() == "N" || yn.Key == ConsoleKey.Escape)
+        return;
+
+    for (int row = parameters.LookupEndRow; row > parameters.LookupStartRow - 1; row--)
+    {
+        string imei = lookupWorksheet.Cell(row, parameters.LookupColumn).Value.ToString();
+        string? result = Search(imei);
+        if (result is null)
+        {
+            if (deleteUnmatchRows)
+                lookupWorksheet.Row(row).Delete();
+        }
+        else
+        {
+            if (!deleteOnly)
+                lookupWorksheet.Cell(row, parameters.LookupOutputColumn).Value = result;
+        }
+    }
+    workbook.Save();
+
+    string? Search(string imei)
+    {
+        for (int row = parameters.SearchStartRow; row < parameters.SearchEndRow + 1; row++)
+        {
+            if (imei == searchWorksheet.Cell(row, parameters.SearchColumn).Value.ToString())
+                return searchWorksheet.Cell(row, parameters.SearchResultColumn).Value.ToString();
+        }
+        return null;
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("Unable to parse all Json parameters");
     return;
-
-for (int row = lookup.EndRow; row > lookup.StartRow - 1; row--)
-{
-    string imei = lookupWorksheet.Cell(row, lookup.Column).Value.ToString();
-    string? result = Search(imei);
-    if (result is null)
-    {
-        if (deleteUnmatchRows)
-            lookupWorksheet.Row(row).Delete();
-    }
-    else
-    {
-        if (!deleteOnly)
-            lookupWorksheet.Cell(row, output_column).Value = result;
-    }
-}
-workbook.Save();
-
-
-string? Search(string imei)
-{
-    for (int row = search.StartRow; row < search.EndRow + 1; row++)
-    {   
-        if (imei == searchWorksheet.Cell(row, search.Column).Value.ToString())
-            return searchWorksheet.Cell(row, result_column).Value.ToString();
-    }
-    return null;
 }
 
-record Range(int Column, int StartRow, int EndRow);
+
+record Range(string Column, int StartRow, int EndRow);
